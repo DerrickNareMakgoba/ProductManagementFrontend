ebfe55e334804a0e3f8aed153469ee97cece845f
Derrick Nare Makgoba <dmakgoba@toyota.co.za>
2026-05-05 17:08:38 +0200

    handle memory leaks

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductStore } from '../store/product-store.service';
+import { distinctUntilChanged, filter } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -11,12 +12,16 @@ export class ProductComponent implements OnInit {
   readonly productStore = inject(ProductStore);
   private pageNumber = 1;
   private pageSize = 3;
+  
 
 
   ngOnInit(): void {
     this.productStore.GetAllProducts();
 
-    this.productStore.hasError$.subscribe((result) => {
+    this.productStore.hasError$
+    .pipe(distinctUntilChanged(),
+      filter(Boolean))
+    .subscribe((result) => {
       if (result) {
         alert('unable to retrive products');
       }
