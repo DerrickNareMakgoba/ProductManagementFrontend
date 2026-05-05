ad4a001bf758847a0a902577dcc2607fdc04a2b6
Derrick Nare Makgoba <dmakgoba@toyota.co.za>
2026-05-05 10:48:50 +0200

    added new pages and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
